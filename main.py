ca389608dfe5f859f0cdb7d3111a863eedd84286
unknown <a.pettenon@yahoo.it>
2021-05-01 23:38:23 +0200

    Version 2

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ tasklist = [["Walk Dog", True], ["Wash Dishes", False], ["Take out Trash", True]
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("tasklist.html", TaskList = tasklist)         #("Hello World !")
+    return render_template("tasklist.html", TaskList = tasklist)         #("Hello World !!!!!!!!!")
 
 app.run()
